977c45affe3f03a4ec6d556378f3e8257d3d02e9
Andrew Flanagan <apflanag@mtu.edu>
2021-10-22 13:13:01 -0400

    add scrolling background

---
+++ Rainbow Ridge/Assets/scroll_background.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scroll_background : MonoBehaviour
+{
+
+    public float scroll_speed = (float) -0.001;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < (float) -1.0)
+        {
+            transform.position = new Vector3(0, 0, 0);
+        }
+            
+        transform.position += new Vector3(0, scroll_speed, 0);
+    }
+}

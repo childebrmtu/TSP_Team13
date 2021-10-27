099f9504acb674eabc52d0deacac7b1290d41bf1
Andrew Flanagan <apflanag@mtu.edu>
2021-10-27 12:43:56 -0400

    add obstacle handler

---
+++ Rainbow Ridge/Assets/obstacle_handler.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obstaclehandler : MonoBehaviour
+{
+
+    public float scroll_speed = (float)-0.001;
+    public float despawn_boundary = (float)-2.0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Transform child in transform)
+        {
+            child.transform.position += new Vector3(0, scroll_speed, 0);
+
+            if (child.transform.position.y < despawn_boundary)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+    }
+}

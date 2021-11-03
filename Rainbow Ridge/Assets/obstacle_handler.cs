e524dc216edaf761e948c23cea11ba5f5f72490c
ajgougeo <ajgougeo@mtu.edu>
2021-11-03 12:48:12 -0400

    Start merging obstacle code from main
    not done yet

---
+++ Rainbow Ridge/Assets/obstacle_handler.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obstacle_handler : MonoBehaviour
+{
+
+    public float scroll_speed = (float)-0.01;
+    public float despawn_boundary = (float)-2.0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        foreach (Transform child in transform)
+        {
+            child.transform.position += new Vector3(0, scroll_speed, 0);
+
+            if (child.transform.position.y < despawn_boundary)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+    }
+}

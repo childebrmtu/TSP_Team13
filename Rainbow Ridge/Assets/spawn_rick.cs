3be22160d5bd07dfb853924640567c4e56849002
ajgougeo <ajgougeo@mtu.edu>
2021-11-03 13:15:31 -0400

    Add prefab, begin merging

--- Rainbow Ridge/Assets/spawn_rick.cs
+++ Rainbow Ridge/Assets/spawn_rick.cs
@@ -10,7 +10,6 @@ public class spawn_rick : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rick = this.transform;
         InvokeRepeating("spawn", (float)0.0, (float)2.0);
     }
 

2b3f255a5a46dcd60a8e036b11578b5fb63d0fd1
ajgougeo <ajgougeo@mtu.edu>
2021-12-10 06:29:18 -0500

    basic music playback, bounds on player movement
    since the music loops to after the intro, I couldn't figure out how to set up the loop easily so we're just looping the part without the intro. Also added really sloppy bounding box on movement but it works

--- Rainbow Ridge/Assets/PlayerController.cs
+++ Rainbow Ridge/Assets/PlayerController.cs
@@ -10,6 +10,11 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D control;
     // placeholder for now
     public float speedFactor = 1;
+    // bounding box vars
+    public float upperX;
+    public float upperY;
+    public float lowerX;
+    public float lowerY;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +29,13 @@ public class PlayerController : MonoBehaviour
         Vector2 inVector = input.actions["Move"].ReadValue<Vector2>();
         inVector.x *= Time.deltaTime * speedFactor;
         inVector.y *= Time.deltaTime * speedFactor;
+
+        // check for bounds very sloppily
+        if (control.position.x >= upperX && inVector.x > 0 || control.position.x <= lowerX && inVector.x < 0)
+            inVector.x = 0;
+        if (control.position.y >= upperY && inVector.y > 0 || control.position.y <= lowerY && inVector.y < 0)
+            inVector.y = 0;
+        
         control.velocity = inVector;
     }
 }
